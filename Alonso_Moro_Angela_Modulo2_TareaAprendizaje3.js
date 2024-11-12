815b0b92e806c43a0e556fb7a1f57f7a6157b60c
angela-moro <analonso@birt.eus>
2024-11-12 13:11:28 +0100

    Ejercicio 1 y 2 corregidos

---
+++ Alonso_Moro_Angela_Modulo2_TareaAprendizaje3.js
@@ -0,0 +1,121 @@
+// Ejercicio 1: Gestión de Lista de Compras (Principiante)
+
+const { clear } = require('console');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// let listado = [];
+
+
+// function elegirModo(){
+//     console.log('Acciones disponibles \n a - Añadir porductos \n d - Eliminar el último producto de la lista \n s - salir');
+//     rl.question('Introduce  la acción: ', (acc) => {
+        
+//         switch (acc.toUpperCase()){
+//             case 'A':
+//                 addProd();
+//                 break;
+//             case 'D':
+//                 delProd();
+//                 break;
+//             case 'S':
+//                 console.log(listado);
+//                 console.log('Hasta  pronto!');
+//                 rl.close();
+//                 break;
+//             default: 
+//                 elegirModo();
+//         }
+//     }); 
+// }
+
+
+// function addProd(){
+//     rl.question('Introduce  un producto o pulsa s: ', (producto) => {
+    
+//         if(producto === 's'){
+//             console.log(listado);
+//             elegirModo();
+//         }else{
+//             listado.push(producto.trim());
+//             addProd();
+//         }
+//     }); 
+// }
+
+// function delProd(){
+//     listado.pop();
+//     console.log(listado);
+//     elegirModo();
+
+// }
+
+// elegirModo();
+
+
+
+// Ejercicio 2: Gestión de Usuarios (Intermedio)
+
+// class Usuario{
+//     constructor (nombre, edad, email){
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.email = email;
+//     }
+
+//     mostrarInfo(){
+//         console.log(`Nombre: ${this.nombre}`);
+//         console.log(`Edad: ${this.edad}`);
+//         console.log(`Email: ${this.email}`);
+//     }
+
+// }
+
+// const usuario = new Usuario ('Hommer', 45, 'amanterechoncho@gmai.com')
+
+// function elegirModo(){
+//     console.log('Acciones disponibles \n c - Crear usuario \n i - Mostrar usuario \n s - salir');
+//     rl.question('Introduce  la acción: ', (acc) => {
+        
+//         switch (acc.toUpperCase()){
+//             case 'C':
+//                 addUser();
+//                 break;
+//             case 'I':
+//                 printUser();
+//                 break;
+//             case 'S':
+//                 console.log('¡Hasta  pronto!');
+//                 rl.close();
+//                 break;
+//             default: 
+//                 elegirModo();
+//         }
+//     }); 
+// }
+
+// function addUser(){
+//     rl.question('Nombre: ', (nom) => {
+//         rl.question('Edad: ', (ed) => {
+//             rl.question('Email: ', (m) => {
+//                 const newUser = new Usuario (nom, ed, m);
+//                 console.log('Usuario creado con éxito');
+//                 newUser.mostrarInfo();
+//                 elegirModo();
+//             })
+//         })
+//     })
+// }
+
+// function printUser(){
+//     usuario.mostrarInfo();
+//     elegirModo();
+// }
+
+// elegirModo();
+
+
+// Ejercicio 3: Procesamiento de Datos JSON y Bucle Avanzado (Avanzado)
